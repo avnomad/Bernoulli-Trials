e600c8614b82ea0b8d5f7d85a6dfadae6259f4fa
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-04-22 17:12:03 +0300

    Added copyright notices and license.
    Also corrected 2 spelling mistakes.

--- Bernoulli Trials/main.cpp
+++ Bernoulli Trials/main.cpp
@@ -1,3 +1,21 @@
+//	Copyright (C) 2007, 2012 Vaptistis Anogeianakis <el05208@mail.ntua.gr>
+/*
+ *	This file is part of Bernoulli Trials.
+ *
+ *	Bernoulli Trials is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Bernoulli Trials is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with Bernoulli Trials.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <iostream>
 using std::wcout;
 using std::wcin;
@@ -14,9 +32,9 @@ int main()
 	
 	wcout << "supply number of random experiments: ";
 	wcin >> n;
-	wcout << "supply least desired number of succesful random experiments: ";
+	wcout << "supply least desired number of successful random experiments: ";
 	wcin >> k;
-	wcout << "supply common posibility of success: ";
+	wcout << "supply common possibility of success: ";
 	wcin >> p;
 
 	BernsteinTriangle bernsteinTriangle(p,n);
